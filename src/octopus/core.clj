e48f3f8b5ec1cb9ae9a43e94248d2725fade2462
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-05-19 10:20:58 +0100

    More threadpool love

--- src/octopus/core.clj
+++ src/octopus/core.clj
@@ -89,3 +89,25 @@
 
 ; we have zero active threads
 (.getActiveCount cached-thread-pool)
+
+; submit a dumb function to the threadpool
+(def keep-running (atom true))
+(.submit cached-thread-pool (fn [] (while @keep-running)))
+
+; we have one active thread
+(.getActiveCount cached-thread-pool)
+
+; see how long idle threads are kept around
+(.getKeepAliveTime cached-thread-pool TimeUnit/MINUTES)
+
+; stop the dumb function from looping
+(swap! keep-running not)
+
+; we have zero active threads
+(.getActiveCount cached-thread-pool)
+
+; we have one completed task
+(.getCompletedTaskCount cached-thread-pool)
+
+; we may have one thread idle
+(.getPoolSize cached-thread-pool)
