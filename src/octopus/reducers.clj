f539b9f21de77ccdff16a66d1a2075b4de46e30b
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-05-19 10:22:51 +0100

    Move things around

--- src/octopus/reducers.clj
+++ src/octopus/reducers.clj
@@ -1,6 +1,5 @@
-(ns octopus.core
-  (:require [clojure.core.reducers :as r])
-  (:import [java.util.concurrent Executors TimeUnit]))
+(ns octopus.reducers
+  (:require [clojure.core.reducers :as r]))
 
 
 ;; Reducers vs. Seq Functions
@@ -79,35 +78,3 @@
 
 ; Then we use reducers with parralelization.
 (time (->> v (r/filter even?) (r/map inc) (r/fold +))) ; 286ms
-
-
-;; Threadpools
-
-
-; we create a threadpool
-(def cached-thread-pool (Executors/newCachedThreadPool))
-
-; we have zero active threads
-(.getActiveCount cached-thread-pool)
-
-; submit a dumb function to the threadpool
-(def keep-running (atom true))
-(.submit cached-thread-pool (fn [] (while @keep-running)))
-
-; we have one active thread
-(.getActiveCount cached-thread-pool)
-
-; see how long idle threads are kept around
-(.getKeepAliveTime cached-thread-pool TimeUnit/MINUTES)
-
-; stop the dumb function from looping
-(swap! keep-running not)
-
-; we have zero active threads
-(.getActiveCount cached-thread-pool)
-
-; we have one completed task
-(.getCompletedTaskCount cached-thread-pool)
-
-; we may have one thread idle
-(.getPoolSize cached-thread-pool)
