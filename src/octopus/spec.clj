519dece1a83fa4f42544b89db38a9a63a4d0fa42
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-06-04 20:02:15 +0100

    Fix up namespace aliases

--- src/octopus/spec.clj
+++ src/octopus/spec.clj
@@ -1,72 +1,73 @@
 (ns octopus.spec
-  (:require [clojure.spec.gen.alpha :as gen]
-            [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.gen.alpha :as spec.gen]
+            [clojure.spec.alpha :as spec]
+            [clojure.spec.test.alpha :as spec.test]))
 
 
 ;; Generators
 
 ; First, obtain a generator from a spec.
-(def some-generator (s/gen #{:a :b :c}))
+(def some-generator (spec/gen #{:a :b :c}))
 
 ; Then generate a single value.
-(gen/generate some-generator)
+(spec.gen/generate some-generator)
 
 ; Or generate multiple values.
-(gen/sample some-generator)
+(spec.gen/sample some-generator)
 
 ; For conformed values, exercise is useful
-(def some-other-generator (s/or :int int? :string string?))
+(def some-other-generator (spec/or :int int? :string string?))
 
-(s/exercise some-other-generator 10)
+(spec/exercise some-other-generator 10)
 
 
 ;; Custom Generators
 
 ; Start with a spec for keywords with a particular namespace.
-(s/def ::my-namespaced-keyword (s/and keyword? #(= (namespace %) "my.domain")))
+(spec/def ::my-namespaced-keyword (spec/and keyword? #(= (namespace %) "my.domain")))
 
 ; The spec works.
-(s/valid? ::my-namespaced-keyword :my.domain/name)
+(spec/valid? ::my-namespaced-keyword :my.domain/name)
 
 ; But it's unlikely to generate a significant match.
-(gen/sample (s/gen ::my-namespaced-keyword))
+(spec.gen/sample (spec/gen ::my-namespaced-keyword))
 
 ; So you can generate from a more narrow generator.
-(gen/sample (s/gen #{:my.domain/name :my.domain/occupation :my.domain/id}))
+(spec.gen/sample (spec/gen #{:my.domain/name :my.domain/occupation :my.domain/id}))
 
 ; You may redefine the original spec in terms of the narrower generator.
-(s/def ::my-namespaced-keyword
-  (s/with-gen (s/and keyword? #(= (namespace %) "my.domain"))
-    #(s/gen #{:my.domain/name :my.domain/occupation :my.domain/id})))
+(spec/def ::my-namespaced-keyword
+  (spec/with-gen (spec/and keyword? #(= (namespace %) "my.domain"))
+    #(spec/gen #{:my.domain/name :my.domain/occupation :my.domain/id})))
 
 ; The spec still works.
-(s/valid? ::my-namespaced-keyword :my.domain/name)
+(spec/valid? ::my-namespaced-keyword :my.domain/name)
 
 ; Now you can generate samples.
-(gen/sample (s/gen ::my-namespaced-keyword))
+(spec.gen/sample (spec/gen ::my-namespaced-keyword))
 
 ; But we've lost flexibility here, by enumerating the generated output, we've constrained ourselves.
 ; We can build up guided generators with fmap.
-(def my-namespaced-keyword-generator (gen/fmap #(keyword "my.domain" %)
-                                               (gen/string-alphanumeric)))
+(def my-namespaced-keyword-generator (spec.gen/fmap #(keyword "my.domain" %)
+                                               (spec.gen/string-alphanumeric)))
 
 ; It works as expected.
-(gen/sample my-namespaced-keyword-generator 5)
+(spec.gen/sample my-namespaced-keyword-generator 5)
 
 ; Putting it all together.
-(s/def ::hello
-  (s/with-gen #(clojure.string/includes? % "hello")
-    #(gen/fmap (fn [[s1 s2]] (str s1 "hello" s2))
-               (gen/tuple (gen/string-alphanumeric) (gen/string-alphanumeric)))))
+(spec/def ::hello
+  (spec/with-gen #(clojure.string/includes? % "hello")
+    #(spec.gen/fmap (fn [[s1 s2]] (str s1 "hello" s2))
+               (spec.gen/tuple (gen/string-alphanumeric) (gen/string-alphanumeric)))))
 
-(gen/sample (s/gen ::hello))
+(spec.gen/sample (spec/gen ::hello))
 
 
 ;; Ranged Generators
 
 ; You can define ranges.
-(s/def ::dice-face (s/int-in 1 7))
+(spec/def ::dice-face (spec/int-in 1 7))
 
 ; Works as expected
-(gen/sample (s/gen ::dice-face))
+(spec.gen/sample (spec/gen ::dice-face))
 
