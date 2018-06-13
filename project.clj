b8fc11ed9d5ef737c10145d6164c155d19180eb6
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-06-13 21:34:22 +0100

    Add Jutsu as a dep

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject octopus "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/test.check "0.9.0"]]
+                 [org.clojure/test.check "0.9.0"]
+                 [hswick/jutsu "0.1.2"]]
   :main ^:skip-aot octopus.core)
