9251e75c176d5486587422ebe171ec8243e26881
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-06-13 23:06:22 +0100

    Add some graphing examples

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject octopus "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
-                 [hswick/jutsu "0.1.2"]]
+                 [hswick/jutsu "0.1.2"]
+                 [clj-time "0.14.4"]]
   :main ^:skip-aot octopus.core)
--- src/octopus/jutsu.clj
+++ src/octopus/jutsu.clj
@@ -1,11 +1,26 @@
 (ns octopus.jutsu
-  (:require [jutsu.core :as jutsu]))
+  (:require [jutsu.core :as jutsu]
+            [clj-time.core :as time]
+            [clj-time.coerce :as time.coerce]
+            [clj-time.format :as time.format]))
+
+; 1525734000000 -- May 8th
+; 1528844400000 -- June 13th
 
 (jutsu/start-jutsu!)
 
+(def instants
+  (with-open [r (clojure.java.io/reader "/Users/ksuzukawa/Downloads/rr2-pub")]
+    (mapv #(Long/parseLong %) (line-seq r))))
+
+(def dates
+  (let [date-formatter (time.format/formatter :date)]
+    (with-open [r (clojure.java.io/reader "/Users/ksuzukawa/Downloads/rr2-eff")]
+      (mapv #(->> % (time.format/parse date-formatter) (time.coerce/to-long)) (line-seq r)))))
+
 (jutsu/graph!
- "Something"
- [{:x [1 2 3 4]
-   :y [1 2 3 4]
+ "rr2"
+ [{:x instants
+   :y (map #(-> %1 (- %2) (/ 86400000) int) instants dates)
    :mode "markers"
    :type "scatter"}])
