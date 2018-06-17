24c4c3cffc34ce11829068b60b2eae2580514e1d
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-06-17 13:31:50 +0100

    More Jitsu magic

--- src/octopus/jutsu.clj
+++ src/octopus/jutsu.clj
@@ -10,18 +10,43 @@
 
 (jutsu/start-jutsu!)
 
-(def instants
-  (with-open [r (clojure.java.io/reader "/Users/ksuzukawa/Downloads/rr2-pub")]
+(defn read-instants [filename]
+  (with-open [r (clojure.java.io/reader filename)]
     (mapv #(Long/parseLong %) (line-seq r))))
 
-(def dates
+(defn read-dates [filename]
   (let [date-formatter (time.format/formatter :date)]
-    (with-open [r (clojure.java.io/reader "/Users/ksuzukawa/Downloads/rr2-eff")]
+    (with-open [r (clojure.java.io/reader filename)]
       (mapv #(->> % (time.format/parse date-formatter) (time.coerce/to-long)) (line-seq r)))))
 
+(defn normalize [xs]
+  (->> xs
+       (mapv #(- % 1525647600000)) ;; normalize to May 7th
+       (remove neg?)
+       (mapv #(/ % 86400000))))
+
+(def lo2 (read-dates "/Users/ksuzukawa/Downloads/lo2"))
+
+(def ld1 (read-instants "/Users/ksuzukawa/Downloads/ld1"))
+
+(def lu1 (read-instants "/Users/ksuzukawa/Downloads/lu1"))
+
+(def lc2 (read-instants "/Users/ksuzukawa/Downloads/lc2"))
+
+(def rr2 (read-instants "/Users/ksuzukawa/Downloads/rr2"))
+
+(def rr3 (read-instants "/Users/ksuzukawa/Downloads/rr3"))
+
+(def lb1 (read-instants "/Users/ksuzukawa/Downloads/lb1"))
+
 (jutsu/graph!
- "rr2"
- [{:x (map #(-> % (- 1525647600000) (/ 86400000) float) instants) ;; 0 is May 7th
-   :y (map #(-> %1 (- %2) (/ 86400000) int) instants dates)
-   :mode "markers"
-   :type "scatter"}])
+ "Timing"
+ (into []
+  (map-indexed
+   (fn [i points]
+     (let [normalized-points (normalize points)]
+       {:x normalized-points
+        :y (repeat (count normalized-points) i)
+        :mode "markers"
+        :type "scatter"}))
+   [lb1 rr3 rr2 lc2 lu1 ld1 lo2])))
