65903a2cca4e0e9a5be3fab80792b48caaccefaa
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-10-12 19:02:45 +0100

    Describe Jutsu and clean up the Threadpools thing

--- src/octopus/jutsu.clj
+++ src/octopus/jutsu.clj
@@ -1,3 +1,22 @@
 (ns octopus.jutsu
   (:require [jutsu.core :as j]))
 
+
+;;  Graphing
+
+; Start Jutsu.
+(j/start-jutsu!)
+
+; Graph this thing in the browser.
+(j/graph!
+ "My First Graph"
+ [{:x [1 2 3 4]
+   :y [1 2 3 4]
+   :mode "markers"
+   :type "scatter"}])
+
+; You can do real time updates.
+(j/update-graph!
+ "My First Graph"
+ {:data {:y [[4]] :x [[5]]}
+  :traces [0]})
--- src/octopus/threadpools.clj
+++ src/octopus/threadpools.clj
@@ -4,30 +4,30 @@
 
 ;; Threadpools
 
-; we create a threadpool
+; We create a threadpool.
 (def cached-thread-pool (Executors/newCachedThreadPool))
 
-; we have zero active threads
+; We have zero active threads.
 (.getActiveCount cached-thread-pool)
 
-; submit a dumb function to the threadpool
+; Submit a dumb function to the threadpool.
 (def keep-running (atom true))
 (.submit cached-thread-pool (fn [] (while @keep-running)))
 
-; we have one active thread
+; We have one active thread.
 (.getActiveCount cached-thread-pool)
 
-; see how long idle threads are kept around
+; See how long idle threads are kept around.
 (.getKeepAliveTime cached-thread-pool TimeUnit/MINUTES)
 
-; stop the dumb function from looping
+; Stop the dumb function from looping.
 (swap! keep-running not)
 
-; we have zero active threads
+; We have zero active threads.
 (.getActiveCount cached-thread-pool)
 
-; we have one completed task
+; We have one completed task.
 (.getCompletedTaskCount cached-thread-pool)
 
-; we may have one thread idle
+; We may have one thread idle.
 (.getPoolSize cached-thread-pool)
